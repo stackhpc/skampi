4a945e4982dc3e07635bedbae812a48d27aac5f5
vla22 <vla22@cam.ac.uk>
2019-11-01 15:46:11 +0000

    Attempt to add progressive waits to make script run faster. Currently broken.

--- scripts/smoketest.sh
+++ scripts/smoketest.sh
@@ -2,11 +2,17 @@
 
 set -e -o pipefail
 echo "pipefail set"
-sleep 4m
+sleep 20s 
 x=$(kubectl get pods -n integration --field-selector=status.phase=Running | wc -l)
 echo "$x"
-if [ $x != "21" ]; then
-	echo "not all pods are running"
-	exit 1
-
+for i in 1 2 3 4 5 6 7 8 9
+do
+	if [ $x = "21" ]; then
+		echo "20 pods running"
+		exit 
+	else
+		sleep 20s
+		echo "waiting ...($i * 20)s"
+echo "not all pods are running"
+exit 1
 fi
