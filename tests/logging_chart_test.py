55eaeaa600117aac243c6bd919003fa9df0afc8e
Sett <swai@ska.ac.za>
2020-01-06 11:44:37 +0200

    Refactor

--- tests/logging_chart_test.py
+++ tests/logging_chart_test.py
@@ -8,12 +8,19 @@ import yaml
 
 @pytest.mark.no_deploy
 def test_pvc_reclaim_policy_is_set_to_recycle(helm_adaptor):
-    pv_resource_def = helm_adaptor.template('logging', give_a_release_name(), 'elastic-pv.yaml')
-    resources_defs = [t for t in yaml.safe_load_all(StringIO(pv_resource_def)) if t is not None]
+    pv_resource_def = helm_adaptor.template('logging', give_any_release_name(), 'elastic-pv.yaml')
+    resources = parse_yaml_str(pv_resource_def)
 
-    assert resources_defs[0]['spec']['persistentVolumeReclaimPolicy'] == 'Recycle'
+    assert resources[0]['spec']['persistentVolumeReclaimPolicy'] == 'Recycle'
 
 
-def give_a_release_name():
-    word = ''.join([random.choice(list(string.ascii_lowercase)) for _ in range(7)])
-    return word
+def parse_yaml_str(pv_resource_def):
+    return [t for t in yaml.safe_load_all(StringIO(pv_resource_def)) if t is not None]
+
+
+def give_any_release_name():
+    return "{}-{}".format(_random_alpha(), _random_alpha())
+
+
+def _random_alpha(length=7):
+    return ''.join([random.choice(list(string.ascii_lowercase)) for _ in range(length)])
