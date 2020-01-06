abe3833ff330a89ede87023e8b4855a26cb5ce46
Sett <swai@ska.ac.za>
2020-01-06 16:30:59 +0200

    Refactor: extract chart templating into fixture

--- tests/logging_chart_test.py
+++ tests/logging_chart_test.py
@@ -1,3 +1,5 @@
+import glob
+import os
 import random
 import string
 from io import StringIO
@@ -6,18 +8,26 @@ import pytest
 import yaml
 
 
+@pytest.fixture(scope="module")
+def chart_templates(helm_adaptor):
+    release_name = give_any_release_name()
+    chart_templates = [os.path.basename(fpath) for fpath in (glob.glob("charts/logging/templates/*.yaml"))]
+
+    rendered_templates = {template: helm_adaptor.template('logging', release_name, template) for template in
+                          chart_templates}
+    return rendered_templates
+
+
 @pytest.mark.no_deploy
-def test_pvc_reclaim_policy_is_set_to_recycle(helm_adaptor):
-    pv_resource_def = helm_adaptor.template('logging', give_any_release_name(), 'elastic-pv.yaml')
-    resources = parse_yaml_str(pv_resource_def)
+def test_pvc_reclaim_policy_is_set_to_recycle(chart_templates):
+    resources = parse_yaml_str(chart_templates['elastic-pv.yaml'])
 
     assert resources[0]['spec']['persistentVolumeReclaimPolicy'] == 'Recycle'
 
 
 @pytest.mark.no_deploy
-def test_elastic_service_is_exposed_on_port_9200_for_all_k8s_nodes(helm_adaptor):
-    elastic_template = helm_adaptor.template('logging', give_any_release_name(), 'elastic.yaml')
-    elastic_svc = parse_yaml_str(elastic_template)[1]
+def test_elastic_service_is_exposed_on_port_9200_for_all_k8s_nodes(chart_templates):
+    elastic_svc = parse_yaml_str(chart_templates['elastic.yaml'])[1]
 
     expected_portmapping = {
         "port": 9200,
@@ -29,21 +39,16 @@ def test_elastic_service_is_exposed_on_port_9200_for_all_k8s_nodes(helm_adaptor)
 
 
 @pytest.mark.no_deploy
-def test_elastic_curator_set_to_run_once_every_hour(helm_adaptor):
-    curator_template = helm_adaptor.template('logging', give_any_release_name(), 'elastic_curator.yaml')
-    curator_job = parse_yaml_str(curator_template).pop()
+def test_elastic_curator_set_to_run_once_every_hour(chart_templates):
+    curator_job = parse_yaml_str(chart_templates['elastic_curator.yaml']).pop()
 
     assert curator_job['spec']['schedule'] == '0 1 * * *'
 
 
 @pytest.mark.no_deploy
-def test_fluentd_is_authorised_to_read_pods_and_namespaces_cluster_wide(helm_adaptor):
-    release_name = give_any_release_name()
-    rbac_template = helm_adaptor.template('logging', release_name, 'fluentd-rbac.yaml')
-    daemonset_template = helm_adaptor.template('logging', release_name, 'fluentd-daemonset.yaml')
-
-    serviceaccount, clusterrole, clusterrolebinding = parse_yaml_str(rbac_template)
-    daemonset = parse_yaml_str(daemonset_template).pop()
+def test_fluentd_is_authorised_to_read_pods_and_namespaces_cluster_wide(chart_templates):
+    serviceaccount, clusterrole, clusterrolebinding = parse_yaml_str(chart_templates['fluentd-rbac.yaml'])
+    daemonset = parse_yaml_str(chart_templates['fluentd-daemonset.yaml']).pop()
     serviceaccount_name = serviceaccount['metadata']['name']
 
     expected_auth_rule = {
@@ -58,13 +63,9 @@ def test_fluentd_is_authorised_to_read_pods_and_namespaces_cluster_wide(helm_ada
 
 
 @pytest.mark.no_deploy
-def test_fluentd_is_configured_to_integrate_with_elastic_via_incluster_hostname(helm_adaptor):
-    release_name = give_any_release_name()
-    fluentd_daemonset_template = helm_adaptor.template('logging', release_name, 'fluentd-daemonset.yaml')
-    elastic_template = helm_adaptor.template('logging', release_name, 'elastic.yaml')
-
-    elastic_deployment, elastic_svc = parse_yaml_str(elastic_template)
-    fluentd_daemonset = parse_yaml_str(fluentd_daemonset_template).pop()
+def test_fluentd_is_configured_to_integrate_with_elastic_via_incluster_hostname(chart_templates):
+    elastic_deployment, elastic_svc = parse_yaml_str(chart_templates['elastic.yaml'])
+    fluentd_daemonset = parse_yaml_str(chart_templates['fluentd-daemonset.yaml']).pop()
 
     expected_env_vars = [
         {"FLUENT_ELASTICSEARCH_HOST": elastic_deployment['metadata']['name']},
