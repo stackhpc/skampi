0fd0e86216e175453b96282849a15dca5833612b
Sett <swai@ska.ac.za>
2020-01-17 15:50:25 +0200

    Increase timeout to 20min (for in-situ troubleshooting)

--- tests/logging_chart_test.py
+++ tests/logging_chart_test.py
@@ -107,7 +107,7 @@ def test_fluentd_ingests_logs_from_pod_stdout_into_elasticsearch(logging_chart_d
     expected_log = "simple were so well compounded"
     echoserver.print_to_stdout(expected_log)
     # TODO solve _wait_until_fluentd_ingests_echoserver_logs for journald-fluentd integration
-    sleep(120)
+    sleep(1200)
     # fluentd_daemonset_name = "daemonset/fluentd-logging-{}".format(logging_chart_deployment.release_name)
     # _wait_until_fluentd_ingests_echoserver_logs(
     #     fluentd_daemonset_name, datetime.now(), test_namespace)
