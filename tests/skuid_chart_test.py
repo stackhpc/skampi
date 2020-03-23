1443dae904d622f580e16ae43c83f4bf9ac70428
SKAJohanVenter <aventer@ska.ac.za>
2020-03-23 10:16:28 +0200

    Added timing tests

--- tests/skuid_chart_test.py
+++ tests/skuid_chart_test.py
@@ -21,15 +21,16 @@ class TestSkuidDeployment:
     def test_skuid_is_up_and_serving(self, skuid_chart_deployment):
         """Check that skuid is up and base path is as expected"""
         skuid_pod_name = skuid_chart_deployment.search_pod_name("skuid-deployment")[0]
-        command_str = "curl -s  -X GET " "http://0.0.0.0:9870/skuid"
 
+        command_str = "curl -s  -X GET http://0.0.0.0:9870/"
         resp = skuid_chart_deployment.pod_exec_bash(skuid_pod_name, command_str)
         assert "Welcome to skuid" in resp
 
-        command_str = "curl -s  -X GET " "http://0.0.0.0:9870/skuid"
+        command_str = "curl -s  -X GET http://0.0.0.0:9870/skuid"
         resp = skuid_chart_deployment.pod_exec_bash(skuid_pod_name, command_str)
+        assert "Welcome to skuid" in resp
 
-        command_str = "curl -s  -X GET " "http://0.0.0.0:9870/skuid/ska_id/test"
+        command_str = "curl -s  -X GET http://0.0.0.0:9870/skuid/ska_id/test"
         resp = skuid_chart_deployment.pod_exec_bash(skuid_pod_name, command_str)
         resp_json = json.loads(resp)
         resp_json = json.loads(resp_json)
@@ -38,21 +39,44 @@ class TestSkuidDeployment:
         assert resp_json["generator_id"] == "T0001"
         assert "test:T0001" in resp_json["ska_uid"]
 
-        command_str = "curl -s  -X GET " "http://0.0.0.0:9870/skuid/ska_scan_id"
+        command_str = "curl -s  -X GET http://0.0.0.0:9870/skuid/ska_scan_id"
         resp = skuid_chart_deployment.pod_exec_bash(skuid_pod_name, command_str)
         resp_json = json.loads(resp)
         resp_json = json.loads(resp_json)
 
-        command_str = "curl -s  -X GET " "http://0.0.0.0:9870/skuid/entity_types/get"
+        command_str = "curl -s  -X GET http://0.0.0.0:9870/skuid/entity_types/get"
         resp = skuid_chart_deployment.pod_exec_bash(skuid_pod_name, command_str)
         resp_json = json.loads(resp)
         resp_json = json.loads(resp_json)
         assert len(resp_json) > 5
 
-        command_str = (
-            "curl -s  -X POST " "http://0.0.0.0:9870/skuid/entity_types/add/new"
-        )
+        command_str = "curl -s  -X POST http://0.0.0.0:9870/skuid/entity_types/add/new"
         resp = skuid_chart_deployment.pod_exec_bash(skuid_pod_name, command_str)
         resp_json = json.loads(resp)
         resp_json = json.loads(resp_json)
         assert "new" in resp_json
+
+    def test_skuid_response_time(self, skuid_chart_deployment):
+        """Ensure the response times are less than 1/20 of a second.
+        NOTE: These timings do not take k8s networking overhead into account
+        """
+        skuid_pod_name = skuid_chart_deployment.search_pod_name("skuid-deployment")[0]
+
+        command_str = (
+            "curl -X GET -L --output /dev/null  --silent --write-out"
+            " '%{time_total}' http://0.0.0.0:9870/"
+        )
+        for path in [
+            "",
+            "skuid",
+            "skuid/ska_id/test",
+            "skuid/ska_scan_id",
+            "skuid/entity_types/get",
+            "skuid/entity_types/add/new",
+        ]:
+            resp = skuid_chart_deployment.pod_exec_bash(
+                skuid_pod_name, command_str + path
+            )
+            logging.info(f"Time for path {path}: {resp}")
+            resp = float(resp)
+            assert resp < 0.05
